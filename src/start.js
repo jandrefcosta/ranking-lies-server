d195b673ce915c89ee36126c3724edf26bd80f4c
João André Figueiredo S. Santos da Costa <Joao2604@Zicard-Accentiv.com.br>
2020-03-12 15:27:41 -0300

    add service ranking list

--- src/start.js
+++ src/start.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => { 
+  res.send('Hello World! ')
+})
+
+app.get('/ranking-list', (req, res) => { 
+  res.json([
+    { resumo: "Minha esposa é pica grossa", points: 150 },
+    { resumo: "Fui sequestrado", points: 115 },
+    { resumo: "Vendi um carro ontem também, igual ao seu", points: 45 }
+  ])
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
